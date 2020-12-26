16d84f89e08407d14f4ba9548985b0f4289a6a15
Sarin <sarincr@yahoo.com>
2020-12-26 11:28:11 +0530

    Frames

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-
 import tkinter as tk 
+from tkinter  import * 
 tks = tk.Tk() 
 tks.geometry('200x300+300+300') 
 tks.title('Calculator') 
 tks.resizable(0,0)
+
+btn1 = Frame(tks, bg="#000000")
+btn1.pack(expand=True, fill="both")
+
+
+btn2 = Frame(tks, bg="#FFFFFF")
+btn2.pack(expand=True, fill="both")
 tks.mainloop() 
