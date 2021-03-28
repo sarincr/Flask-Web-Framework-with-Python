bb1cf3cbb74bca9b2b91df3f08be472a9f3b9766
sarincr <sarinrays@gmail.com>
2021-03-28 23:48:31 +0530

    Button Command

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ tks.resizable(0,0)
 tks.title('Calculator') 
 
 
+val = ""
+
+
+
 dt = StringVar()
 lb = Label(
     tks,
@@ -17,12 +21,61 @@ lb = Label(
     textvariable = dt,
     background = "#ffffff",
     fg = "#000000",
-
-    
-
 )
 
 lb.pack(expand = True, fill = "both")
+
+
+def btn1CK():
+    global val
+    val = val + "1"
+    dt.set(val)
+
+def btn2CK():
+    global val
+    val = val + "2"
+    dt.set(val)
+
+def btn3CK():
+    global val
+    val = val + "3"
+    dt.set(val)
+
+def btn4CK():
+    global val
+    val = val + "4"
+    dt.set(val)
+
+def btn5CK():
+    global val
+    val = val + "5"
+    dt.set(val)
+
+def btn6CK():
+    global val
+    val = val + "6"
+    dt.set(val)
+
+def btn7CK():
+    global val
+    val = val + "7"
+    dt.set(val)
+
+def btn8CK():
+    global val
+    val = val + "8"
+    dt.set(val)
+
+def btn9CK():
+    global val
+    val = val + "9"
+    dt.set(val)
+
+def btn0CK():
+    global val
+    val = val + "0"
+    dt.set(val)
+
 
 
 
@@ -46,19 +99,22 @@ btn4.pack(expand=True, fill="both")
 
 b1 = Button(btn1, text = "1", font = ("Verdana", 10),
     relief = GROOVE,
-    border = 0
+    border = 0,
+    command = btn1CK
 )
 b1.pack(side = LEFT, expand = True, fill = "both",)
 
 b2 = Button(btn1, text = "2", font = ("Verdana", 10),
     relief = GROOVE,
-    border = 0
+    border = 0,
+    command = btn2CK
 )
 b2.pack(side = LEFT, expand = True, fill = "both",)
 
 b3 = Button(btn1, text = "3", font = ("Verdana", 10),
     relief = GROOVE,
-    border = 0
+    border = 0,
+    command = btn3CK
 )
 b3.pack(side = LEFT, expand = True, fill = "both",)
 
@@ -70,19 +126,22 @@ b4.pack(side = LEFT, expand = True, fill = "both",)
 
 b5 = Button(btn2, text = "4", font = ("Verdana", 10),
     relief = GROOVE,
-    border = 0
+    border = 0,
+    command = btn4CK
 )
 b5.pack(side = LEFT, expand = True, fill = "both",)
 
 b6 = Button(btn2, text = "5", font = ("Verdana", 10),
     relief = GROOVE,
-    border = 0
+    border = 0,
+    command = btn5CK
 )
 b6.pack(side = LEFT, expand = True, fill = "both",)
 
 b7 = Button(btn2, text = "6", font = ("Verdana", 10),
     relief = GROOVE,
-    border = 0
+    border = 0,
+    command = btn6CK
 )
 b7.pack(side = LEFT, expand = True, fill = "both",)
 
@@ -96,19 +155,22 @@ b8.pack(side = LEFT, expand = True, fill = "both",)
 
 b9 = Button(btn3, text = "7", font = ("Verdana", 10),
     relief = GROOVE,
-    border = 0
+    border = 0,
+    command = btn7CK
 )
 b9.pack(side = LEFT, expand = True, fill = "both",)
 
 b10 = Button(btn3, text = "8", font = ("Verdana", 10),
     relief = GROOVE,
-    border = 0
+    border = 0,
+    command = btn8CK
 )
 b10.pack(side = LEFT, expand = True, fill = "both",)
 
 b11 = Button(btn3, text = "9", font = ("Verdana", 10),
     relief = GROOVE,
-    border = 0
+    border = 0,
+    command = btn9CK
 )
 b11.pack(side = LEFT, expand = True, fill = "both",)
 
@@ -120,7 +182,8 @@ b12.pack(side = LEFT, expand = True, fill = "both",)
 
 b13 = Button(btn4, text = "0", font = ("Verdana", 10),
     relief = GROOVE,
-    border = 0
+    border = 0,
+    command = btn0CK
 )
 b13.pack(side = LEFT, expand = True, fill = "both",)
 
